4d30ad231161bc9c0ccd577321174a7a0b138a81
shivani18051996 <shivanitrivedi17@gmail.com>
2024-05-04 22:53:54 +0530

    feat: Changes In CustomerService File

--- src/CustomerService.js
+++ src/CustomerService.js
@@ -2,26 +2,26 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://78.46.203.31:49160/api/customers';
 
-class CustomerService {
+const CustomerService = {
   getAllCustomers() {
     return axios.get(API_BASE_URL);
-  }
+  },
 
   createCustomer(customer) {
     return axios.post(API_BASE_URL, customer);
-  }
+  },
 
   getCustomerById(customerId) {
     return axios.get(`${API_BASE_URL}/${customerId}`);
-  }
+  },
 
   updateCustomer(customerId, customer) {
     return axios.put(`${API_BASE_URL}/${customerId}`, customer);
-  }
+  },
 
   deleteCustomer(customerId) {
     return axios.delete(`${API_BASE_URL}/${customerId}`);
   }
-}
+};
 
-export default new CustomerService();
+export default CustomerService;
